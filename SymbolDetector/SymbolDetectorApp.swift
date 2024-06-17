d388abaa5b2a31d8e9621bc2c999938589c28ea4
weichen-irvinesoft <164918815+weichen-irvinesoft@users.noreply.github.com>
2024-06-17 09:54:12 -0700

    Initial Commit

---
+++ SymbolDetector/SymbolDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolDetectorApp.swift
+//  SymbolDetector
+//
+//  Created by Wei Chen on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SymbolDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
